2d991b75a8ceb95d69d43a2a53a64c0a7ea8fb2f
Li Haotian <2878348186@qq.com>
2023-11-22 22:58:52 +0800

    struct change

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # ai technology lab
-## clone me
+## 1.clone me and cd
 ```
-git clone https://github.com/Lilinzta/ai.git
+git clone https://github.com/Lilinzta/ai.git && cd ai
+```
+## 2.create a virtual enveriment
+```sh
+python -m venv venv
+```
+## 3.activate a virtual enveriment
+```
+source venv/bin/activate
+```
+## 4.install requirements
+```
+pip install -r requirements.txt
+```
+## 5.run
+```
+lab1: python lab1/main.py
+lab2: python lab2/main.py
+lab3: python lab3/main.py
 ```
 ## lab1: [KNN demo](https://github.com/Lilinzta/ai/blob/main/lab1/main.py)
 
--- lab3/main.py
+++ lab3/main.py
@@ -5,30 +5,37 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-# 加载加利福尼亚州房价数据集
-housing_22 = fetch_california_housing()
-X_22, y_22 = housing_22.data, housing_22.target
 
-# 划分数据集为训练集和测试集
-X_train_22, X_test_22, y_train_22, y_test_22 = train_test_split(
-    X_22, y_22, test_size=0.2, random_state=42)
+def main():
 
-# 初始化线性回归模型
-model_22 = LinearRegression()
+    # 加载加利福尼亚州房价数据集
+    housing_22 = fetch_california_housing()
+    X_22, y_22 = housing_22.data, housing_22.target
 
-# 训练模型
-model_22.fit(X_train_22, y_train_22)
+    # 划分数据集为训练集和测试集
+    X_train_22, X_test_22, y_train_22, y_test_22 = train_test_split(
+        X_22, y_22, test_size=0.2, random_state=42)
 
-# 在测试集上进行预测
-y_pred_22 = model_22.predict(X_test_22)
+    # 初始化线性回归模型
+    model_22 = LinearRegression()
 
-# 评估模型性能
-mse_22 = mean_squared_error(y_test_22, y_pred_22)
-print(f'Mean Squared Error on Test Set: {mse_22}')
+    # 训练模型
+    model_22.fit(X_train_22, y_train_22)
 
-# 绘制真实值与预测值的对比图
-plt.scatter(y_test_22, y_pred_22)
-plt.xlabel('True Values')
-plt.ylabel('Predictions')
-plt.title('True Values vs. Predictions')
-plt.show()
+    # 在测试集上进行预测
+    y_pred_22 = model_22.predict(X_test_22)
+
+    # 评估模型性能
+    mse_22 = mean_squared_error(y_test_22, y_pred_22)
+    print(f'Mean Squared Error on Test Set: {mse_22}')
+
+    # 绘制真实值与预测值的对比图
+    plt.scatter(y_test_22, y_pred_22)
+    plt.xlabel('True Values')
+    plt.ylabel('Predictions')
+    plt.title('True Values vs. Predictions')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
